d7ab0909822beaabbe366c82fe2bb45dabcca829
Luke Lampman <lukeb.lampman@gmail.com>
2021-05-18 15:50:37 -0500

    add gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 const {src, dest, series, parallel} = require('gulp');
 const uglify = require('gulp-uglify')
 const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 const htmlTask = function(){
   return src('src/*.html').pipe(dest('dist/'));
 };
 const styleTask = function(){
   return src('src/*.css')
+  .pipe(concat('styles.css'))
   .pipe(cleanCSS())
   .pipe(dest('dist/'));
 };
